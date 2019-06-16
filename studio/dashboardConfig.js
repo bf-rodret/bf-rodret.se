8ae9c0e3042963605eb48f42cb7e846e7778ef9d
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2019-06-16 17:26:13 +0200

    Adds dashboard

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'BF Rodret Site',
+            apiId: '17e9cd86-6312-4bac-a47a-ac5c12505f83',
+            buildHookId: '5d065b11052fc0be9653f23a',
+            name: 'bf-rodret'
+          }
+        ]
+      }
+    }
+  ]
+}
