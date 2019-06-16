4b0dc6b8847138312fc0836862c7b964ddd667ef
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2019-06-16 16:45:25 +0200

    Builds successfully

--- web/client.js
+++ web/client.js
@@ -1,6 +1,6 @@
-import sanityClient from '@sanity/client'
+const sanityClient = require('@sanity/client')
 
-export default sanityClient({
+module.exports = sanityClient({
   projectId: 'bjnjq7k1', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,30 @@
 const withSass = require('@zeit/next-sass')
+const client = require('./client')
 
-module.exports = withSass()
+module.exports = withSass({
+  useFileSystemPublicRoutes: false,
+  // Make sure that your node enviroment supports async/await
+  exportPathMap: async function (defaultPathMap) {
+    const path = await client
+      // get all the history articles and return those with slugs
+      .fetch('*[_type == "historyArticle"].slug.current')
+      .then(function(data) {
+        // use reduce to build an object with routes
+        // and select the history-article.js file
+        const paths = data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+            '/om-huset': { page: '/om-huset' },
+            '/om-huset/tidslinje': { page: '/timeline' },
+            ...acc,
+            [`/om-huset/${slug}`]: { page: '/history-article', query: { slug } }
+          }),
+          {}
+        )
+        console.log(paths)
+        return paths
+      })
+      .catch(console.error)
+    return path
+  }
+})
