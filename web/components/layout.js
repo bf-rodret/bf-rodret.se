a6143b2d4b9e70a3ecd6406beda5815e3ca6c666
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2020-02-23 17:33:07 +0100

    Design fixes

--- web/components/layout.js
+++ web/components/layout.js
@@ -6,7 +6,7 @@ export default props => (
     <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link href="https://fonts.googleapis.com/css?family=Montserrat:700,500|PT+Serif:400,400i&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Lexend+Zetta|Montserrat:700,500|PT+Serif:400,400i&display=swap" rel="stylesheet" />
       <title>Bf Rodret UPA</title>
     </Head>
     <div className="main">
