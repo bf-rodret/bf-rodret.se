e496401643f68caf88fb0be4abc894c41acddf16
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2020-05-11 23:31:24 +0200

    Updates next config

--- web/next.config.js
+++ web/next.config.js
@@ -5,25 +5,32 @@ module.exports = withSass({
   useFileSystemPublicRoutes: false,
   // Make sure that your node enviroment supports async/await
   exportPathMap: async function (defaultPathMap) {
-    const path = await client
-      // get all the history articles and return those with slugs
-      .fetch('*[_type == "historyArticle"].slug.current')
-      .then(function(data) {
-        // use reduce to build an object with routes
-        // and select the history-article.js file
-        const routes = data.reduce(
-          (acc, slug) => ({
-            '/': { page: '/' },
-            '/om-huset': { page: '/om-huset' },
-            '/om-huset/tidslinje': { page: '/timeline' },
-            ...acc,
-            [`/om-huset/${slug}`]: { page: '/history-article', query: { slug } }
-          }),
-          {}
-        )
-        return routes
-      })
-      .catch(console.error)
-    return path
+    const historyArticles = await client.fetch('*[_type == "historyArticle"].slug.current')
+    const historyImages = await client.fetch(`*[_type == "historyImage"]._id`)
+
+    const routes = {
+      '/': { page: '/' },
+      '/om-huset': { page: '/om-huset' },
+      '/om-huset/bilder': { page: '/images' },
+      '/om-huset/tidslinje': { page: '/timeline' },
+    }
+
+    Object.assign(routes, historyArticles.reduce(
+      (acc, slug) => ({
+        ...acc,
+        [`/om-huset/${slug}`]: { page: '/history-article', query: { slug } }
+      }),
+      {}
+    ))
+
+    Object.assign(routes, historyImages.reduce(
+      (acc, _id) => ({
+        ...acc,
+        [`/om-huset/bilder/${_id}`]: { page: '/image', query: { id: _id } }
+      }),
+      {}
+    ))
+
+    return routes
   }
 })
