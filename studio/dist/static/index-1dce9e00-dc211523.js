a99401e5969df38ead2f593e0f57adb4a3fe1b63
Martin Lissmyr <martin@lissmyr.com>
2023-09-22 14:08:10 +0200

    Updates sanity

---
+++ studio/dist/static/index-1dce9e00-dc211523.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-504fa488-36f71545.js";import{D as m}from"./sanity-ac3ba962.js";import"./index-600613d5.js";export{m as DocumentPaneProvider,t as default};
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,9 @@
+// sanity.cli.js
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "bjnjq7k1",
+    dataset: "production",
+  }
+});
