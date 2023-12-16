f21ff6055fce115c65e906e0658be45b71dd1675
Martin Lissmyr <martin@lissmyr.com>
2023-12-16 13:19:48 +0100

    Booking page calendar

--- web/components/calendar.tsx
+++ web/components/calendar.tsx
@@ -4,6 +4,7 @@ import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import moment from 'moment';
 import 'moment/locale/sv';
 import DragDropContext from 'components/dragdropcontext';
+import { useMemo, useCallback } from 'react';
 
 export const ResourceCalendar = ({bookings = [], start = 0, end = 24}) => {
   moment.locale('sv');
@@ -11,21 +12,61 @@ export const ResourceCalendar = ({bookings = [], start = 0, end = 24}) => {
 
   const today = new Date();
 
-  const onSelectSlot = (slotInfo) => {
-    console.log(slotInfo);
-  }
+  const onSelectSlot = useCallback(
+      (slotInfo) => {
+      if (slotInfo.action == "doubleClick") return; // cancel dbl click 
+      if (slotInfo.end < new Date()) return; // ends in the past
+      console.log(slotInfo);
+    },
+    []
+  );
+
+  const messages = useMemo(
+    () => ({
+      date: 'Datum',
+      time: 'Tid',
+      event: 'Bokning',
+      allDay: 'Heldag',
+      week: 'Vecka',
+      work_week: 'Work Week',
+      day: 'Dag',
+      month: 'Månad',
+      previous: '<',
+      next: '>',
+      yesterday: 'Igår',
+      tomorrow: 'Imorgon',
+      today: 'Idag',
+      agenda: 'Agenda',
+      noEventsInRange: 'Inga bokningar denna period.',
+      showMore: total => `+${total} fler`,
+    }),
+    []
+  );
+
+  const tileDisabled = useCallback(
+    (date) => {
+      if (date < new Date()) {
+        return {
+          className: "rbc-custom__past-time"
+        }
+      }
+    },
+    []
+  );
 
   return (
     <Calendar
       localizer={localizer}
       defaultView={Views.WEEK}
+      slotPropGetter={tileDisabled}
       views={[
         Views.DAY,
         Views.WEEK
       ]}
       events={bookings}
-      step={120}
+      step={60}
       timeslots={1}
+      messages={messages}
       min={
         new Date(
           today.getFullYear(), 
